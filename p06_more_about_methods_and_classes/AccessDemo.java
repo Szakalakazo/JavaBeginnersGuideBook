151d197fad18376c1163d57389912fb44f28bee9
Szakalakazo <tomek.starzak@gmail.com>
2019-01-09 17:23:48 +0100

    Initial commit

---
+++ p06_more_about_methods_and_classes/AccessDemo.java
@@ -0,0 +1,23 @@
+package p06_more_about_methods_and_classes;
+
+class MyClass {
+    private int alpha;
+    private int beta;
+    int gamma;
+
+    void setAlpha(int alpha) {
+        this.alpha = alpha;
+    }
+
+    int getAlpha() {
+        return alpha;
+    }
+}
+
+public class AccessDemo {
+    public static void main(String[] args) {
+        MyClass ob = new MyClass();
+        ob.setAlpha(99);
+        //ob.beta = 77 won' work!!
+    }
+}
